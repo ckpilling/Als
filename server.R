ae40d844cbfa730f6fc26be4388d5475ff9770d8
ckpilling <christopher.pilling@uk.fujitsu.com>
2019-07-07 11:27:42 +0100

    m

--- server.R
+++ server.R
@@ -44,7 +44,7 @@ shinyServer(function(input, output, session) {
     if (set_random_cities() == 0 & map_name() == "usa") return()
     run_annealing_process$suspend()
     
-    run_annealing_process$suspend()
+    # run_annealing_process$suspend()
 
     isolate({
       if (map_name() == "world") {
