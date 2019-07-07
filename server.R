d8115c224a4d8636a5a2313a2e97f5b18104ebb9
ckpilling <christopher.pilling@uk.fujitsu.com>
2019-07-07 13:11:05 +0100

    m

--- server.R
+++ server.R
@@ -26,9 +26,9 @@ shinyServer(function(input, output, session) {
   update_allowed_cities = observe({
     # if (isolate(input$go_button) == 0 & isolate(set_random_cities()) == 0 & map_name() == "usa") return()
     
-    input$go_button == 0
-    set_random_cities() == 0
-    map_name() == "usa"
+    # input$go_button == 0
+    # set_random_cities() == 0
+    # map_name() == "usa"
 
     updateSelectizeInput(session, "cities", choices=city_choices()$full.name)
    }, priority=500)
